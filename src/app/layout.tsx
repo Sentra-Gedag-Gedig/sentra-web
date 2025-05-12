d7963b7ed6f8faee96da7198981df999d5224815
JasonnSW <jasonsurya17@gmail.com>
2025-05-13 02:54:56 +0700

    Refactor and add new features for KTP functionality
    - Renamed page component from 'page' to 'Face' in src/app/face/page.tsx
    - Added new KTP page with header and capture components in src/app/ktp/page.tsx
    - Updated layout metadata to include custom icon in src/app/layout.tsx
    - Removed obsolete money detection page in src/app/money-detection/page.tsx
    - Updated import path for utility function in src/shared/ui/card.tsx
    - Created new QRIS page in src/app/qris/page.tsx
    - Implemented header component for KTP in src/features/ktp/components/header.tsx
    - Developed KTP capture component with video stream functionality in src/features/ktp/components/ktp-capture.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Sentra",
   description: "Sentra",
+  icons: {
+    icon: "/Sentra.svg",
+  },
 };
 
 export default function RootLayout({
--- src/app/qris/page.tsx
+++ src/app/qris/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Face() {
+export default function Qris() {
   return (
     <div>
-      hello
+      
     </div>
   )
 }
